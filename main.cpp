b8f94ab7b2866dec319c5f423f0b3ea760a4f31b
vriela36 <gfron@student.agh.edu.pl>
2025-03-10 14:27:35 +0100

    main updated - subtract

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ int subtract(int a, int b) {
 }
 
 int main() {
-  cout << "Witaj w naszym programie!" << endl;
-  return 0;
-} 
-
-
+    cout << "Witaj w naszym programie!" << endl;
+    cout << "5 - 3 = " << subtract(5, 3) << endl;
+    return 0;
+}
